73b1a3cb9502a00f4cc0d9b0324bf49873f48383
Shuheng Wang <swang157@jhu.edu>
2017-01-13 14:38:50 -0500

    saving new readwrite and input files

---
+++ readwrite.py
@@ -0,0 +1,29 @@
+def read():
+	f=open("input",'r')
+
+	#read a
+	line=f.readline()
+	split=line.split()	
+	print(split)
+	a=split[2]
+	print(a)
+
+	#read b
+	line=f.readline()
+	split=line.split()	
+	print(split)
+	a=split[2]
+	print(a)
+	
+	#read c
+	line=f.readline()
+	split=line.split()	
+	print(split)
+	a=split[2]
+	print(a)
+	
+	#clean up
+	f.close(
+
+	#return a, b, c
+	return [a,b,c])
